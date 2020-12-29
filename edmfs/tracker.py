16c0cd2e671f69d8b0f9e8827cfc21208a566464
Anthony Langsworth <anthony.langsworth@global.ntt>
2020-12-29 19:34:11 +1100

    Add *State classes

--- edmfs/tracker.py
+++ edmfs/tracker.py
@@ -1,6 +1,44 @@
 from typing import Dict, Any
 from abc import ABC, abstractmethod
-from typing import Dict, Any
+
+class StarSystemState:
+    def __init__(self, name:str, address:int, minor_factions:tuple):
+        self._name = name
+        self._address = address
+        self._minor_factions = minor_factions
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def address(self) -> str:
+        return self._address
+
+    @property
+    def minor_factions(self) -> str:
+        return self._minor_factions
+
+class GalaxyState:
+    def __init__(self):
+        self._systems:Dict[int, StarSystemState] = {}
+
+    @property
+    def systems(self) -> list:
+        return self._systems
+
+class PilotState:
+    def __init__(self):
+        self._last_docked_station = None
+        self._missions:list = []
+
+    @property
+    def last_docked_station(self) -> object:
+        return self._last_docked_station
+
+    @property
+    def missions(self) -> list:
+        return self.missions
 
 class EventProcessor(ABC):
     @property
@@ -9,7 +47,7 @@ class EventProcessor(ABC):
         pass
 
     @abstractmethod
-    def process(self, entry:Dict[str, Any]) -> None:
+    def process(self, entry:Dict[str, Any], star_system_state:StarSystemState) -> None:
         pass
     
 # Eventually, move this to an IoC setup
@@ -21,13 +59,23 @@ _eventProcessors:Dict[str, EventProcessor] = {
 class Tracker:
     def __init__(self, minor_faction:str):
         self._minor_faction:str = minor_faction
+        self._pilot_state:PilotState = None
+        self._galaxy_state:GalaxyState = None
     
     @property
     def minor_faction(self) -> str:
         return self._minor_faction
 
-    def on_entry(self, entry:Dict[str, Any], state:Dict[str, Any]):
-        eventProcessor:EventProcessor = _eventProcessors.get(entry["event"], None)
+    @property
+    def pilot_state(self) -> PilotState:
+        return self._pilot_state
+
+    @property
+    def galaxy_state(self) -> GalaxyState:
+        return self._galaxy_state
+
+    def on_entry(self, entry:Dict[str, Any]):
+        eventProcessor:EventProcessor = _eventProcessors.get(entry["event"], self._pilot_state, self._galaxy_state)
         if eventProcessor != None:
             eventProcessor.process(entry)
         return None
