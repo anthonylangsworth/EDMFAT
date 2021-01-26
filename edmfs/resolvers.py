4f896775f35fd77d67610c41918edab1b5a2dab3
Anthony Langsworth <anthony.langsworth@global.ntt>
2021-01-26 17:30:16 +1100

    Collapse method

--- edmfs/resolvers.py
+++ edmfs/resolvers.py
@@ -5,13 +5,9 @@ from .state import StarSystem
 def resolve_star_system_via_edsm(system_address:int) -> StarSystem:
     URL = "https://www.edsm.net/api-system-v1/factions"
     star_system = None
-
-    post = {
-        "systemId": system_address
-    }
-    response = requests.post(URL, json=post)
+    response = requests.get(URL, params={ "systemId64": system_address })
     if response.status_code == 200:
         output = response.json()
         minor_factions = [faction["name"] for faction in output["factions"]]
-        star_system = StarSystem(response.name, system_address, minor_factions) 
+        star_system = StarSystem(output["name"], system_address, minor_factions) 
     return star_system
