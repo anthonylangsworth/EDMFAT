c4e7cf13a55901b08b03462f2a8ad57d11f80c60
Anthony Langsworth <anthony.langsworth@global.ntt>
2021-01-31 17:29:53 +1100

    Expand event summary

--- edmfs/serializers.py
+++ edmfs/serializers.py
@@ -1,13 +1,32 @@
 import json
+from typing import Dict
 
+from .event_summaries import EventSummary
+from .state import Mission
 from .tracker import Tracker
 
-def serialize_tracker_v1(tracker:Tracker):
+def serialize_event_summary_v1(event_summary:EventSummary) -> Dict:
+    
+
+    return {
+        "type": type(event_summary)
+        "event_summary": 
+    }
+
+def serialize_mission_v1(mission:Mission) -> Dict:
+    return {
+        "id": mission.id,
+        "minor_faction": mission.minor_faction,
+        "influence": mission.influence,
+        "system_address": mission.system_address
+    }
+
+def serialize_tracker_v1(tracker:Tracker) -> Dict:
     result = {
         "version": 1,
         "tracker": {
             "pilot_state": {
-                "missions": [mission for mission in tracker._pilot_state.missions]
+                "missions": [serialize_mission_v1(mission) for mission in tracker._pilot_state.missions]
             },
             "event_summaries": [event_summary for event_summary in tracker._event_summaries]
         }
@@ -15,4 +34,7 @@ def serialize_tracker_v1(tracker:Tracker):
     return result
 
 def serialize_tracker(tracker:Tracker) -> str:
-    return json.dumps(serialize_tracker_v1)
+    return json.dumps({
+        "version": 1,
+        "tracker": serialize_tracker_v1(tracker)
+    })
