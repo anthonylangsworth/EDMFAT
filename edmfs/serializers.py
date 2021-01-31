a496b6fec8bdc5f93bb37fc46fa01bfcfc4aa1e6
Anthony Langsworth <anthony.langsworth@global.ntt>
2021-01-31 19:21:47 +1100

    Adding event summaries

--- edmfs/serializers.py
+++ edmfs/serializers.py
@@ -1,12 +1,49 @@
 import json
 from typing import Dict
 
-from .event_summaries import EventSummary
+from .event_summaries import EventSummary, RedeemVoucherEventSummary, SellExplorationDataEventSummary, MarketSellEventSummary, MissionCompletedEventSummary
 from .state import Mission
 from .tracker import Tracker
 
+def serialize_event_summary(event_summary:EventSummary) -> Dict:
+    return {
+        "minor_faction": redeem_voucher_event_summary.minor_faction,
+        "supports": redeem_voucher_event_summary.supports,
+        "system_name": redeem_voucher_event_summary.system_name,
+    }
+
+def serialize_redeem_voucher_event_summary(redeem_voucher_event_summary:RedeemVoucherEventSummary) -> Dict:
+    return {
+        **serialize_event_summary(redeem_voucher_event_summary),
+        **{
+            "voucher_type": redeem_voucher_event_summary.voucher_type,
+        }
+    }
+
+def serialize_sell_exploration_data_event_summary(sell_exploration_data_event_summary:SellExplorationDataEventSummary) -> Dict:
+    return {
+        **serialize_event_summary(redeem_voucher_event_summary),
+        **{
+            "amount": sell_exploration_data_event_summary.amount
+        }
+    }    
+
+def serialize_market_sell_event_summary(market_sell_event_summary:SellExplorationDataEventSummary) -> Dict:
+    return {
+        **serialize_event_summary(redeem_voucher_event_summary),
+        **{
+            "amount": sell_exploration_data_event_summary.amount
+        }
+    }    
+
+
 def serialize_event_summary_v1(event_summary:EventSummary) -> Dict:
-    
+    event_summary_serializers = {
+        "RedeemVoucherEventSummary": serialize_redeem_voucher_event_summary,
+        "SellExplorationDataEventSummary": serialize_sell_exploration_data_event_summary,
+        "MarketSellEventSummary": None,
+        "MissionCompletedEventSummary": None
+    }
 
     return {
         "type": type(event_summary)
@@ -37,4 +74,4 @@ def serialize_tracker(tracker:Tracker) -> str:
     return json.dumps({
         "version": 1,
         "tracker": serialize_tracker_v1(tracker)
-    })
+    }, indent=4)
