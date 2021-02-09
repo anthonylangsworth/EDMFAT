32ec22ea036f2294677e91e5374af26bd3724d7c
Anthony Langsworth <anthony.langsworth@global.ntt>
2021-02-09 14:21:52 +1100

    Add timestamp

--- wix/signmsi.cmd
+++ wix/signmsi.cmd
@@ -1,1 +1,1 @@
-signtool sign /sha1 13e3cc84a91445eda3cb88d4915c888120a823f1 edmfat.msi
+signtool sign /tr http://timestamp.digicert.com /fd sha256 /td sha256 /sm /sha1 13e3cc84a91445eda3cb88d4915c888120a823f1 /d "EDMFAT MSI" edmfat.msi
