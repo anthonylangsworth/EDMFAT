45d86c9d4f7961fda80a5f4c782291fdb2925a69
Anthony Langsworth <anthony.langsworth@global.ntt>
2021-02-15 12:03:30 +1100

    Add release check

--- web_services.py
+++ web_services.py
@@ -6,7 +6,6 @@ from edmfs.state import StarSystem
 
 def resolve_star_system_via_edsm(logger: logging.Logger, system_address:int) -> StarSystem:
     URL = "https://www.edsm.net/api-system-v1/factions"
-    star_system = None
     try:
         response = requests.get(URL, params={ "systemId64": system_address }, timeout=30)
         if response.status_code == 200:
@@ -15,28 +14,28 @@ def resolve_star_system_via_edsm(logger: logging.Logger, system_address:int) -> 
                 minor_factions = [faction["name"] for faction in output["factions"]]
                 star_system = StarSystem(output["name"], system_address, minor_factions) 
                 logger.info(f"Resolved from EDSM: { star_system }")
+                return star_system
             else:
                 raise Exception(f"Response mising 'factions' or 'output': {output}")
     except Exception as e:
         logger.exception(f"Error resolving { system_address } from EDSM: { e }")        
-    return star_system
+        raise
 
 # See https://docs.github.com/en/rest/reference/repos#get-the-latest-release
-def get_latest_release(logger: logging.Logger, current_version:Tuple[int], owner:str, repo:str):
+def get_latest_release(logger: logging.Logger, owner:str, repo:str) -> Tuple:
     URL = f"/repos/{owner}/{repo}/releases/latest"
     try:
         response = requests.get(URL, headers={"accept":"application/vnd.github.v3+json"}, timeout=30)
         if response.status_code == 200:
             output = response.json()
-            if "tag_name" in output and "url" in output:
-                # logger.info(f"Resolved from EDSM: { star_system }")
-                pass
-            else:
-                raise Exception(f"Response mising 'tag_name' or 'url': {output}")
+            return (output["tag_name"], output["url"])
     except Exception as e:
         logger.exception(f"Error: { e }")        
-    return []
+        raise
 
 def split_tag(tag:str) -> Tuple[int]:
     return tuple(map(int, tag.lstrip("v").split(".")))
 
+def is_later_release_available(logger: logging.Logger, owner:str, repo:str, current_version:Tuple[int]) -> str:
+    tag_name, url = get_latest_release(logger, owner, repo)
+    return url if split_tag(tag_name) > current_version else None
