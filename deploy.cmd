bf00ba61ebb45c3bb626d16eb8f4efcdd3921a03
Anthony Langsworth <anthony.langsworth@global.ntt>
2021-02-27 16:44:34 +1100

    Add error handling

--- deploy.cmd
+++ deploy.cmd
@@ -1,9 +1,17 @@
 set dest=%userprofile%\AppData\Local\EDMarketConnector\plugins\EDMFAT
-copy %dest%\settings.json %temp%
-rd /s /q %dest%
-mkdir %dest%
-xcopy /y load.py %dest%
-xcopy /y web_services.py %dest%
-mkdir %dest%\edmfs
-xcopy /s /y edmfs\*.py %dest%\edmfs 
-copy %temp%\settings.json %dest%
+copy %dest%\settings.json %temp% || goto error
+rd /s /q %dest% || goto error
+mkdir %dest% || goto error
+xcopy /y load.py %dest% || goto error
+xcopy /y web_services.py %dest% || goto error
+mkdir %dest%\edmfs || goto error
+xcopy /s /y edmfs\*.py %dest%\edmfs  || goto error
+copy %temp%\settings.json %dest% || goto error
+
+goto end
+
+:error
+echo Error %errorlevel%
+exit /b %errorlevel%
+
+:end
