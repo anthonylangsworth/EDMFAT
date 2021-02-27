5a0b7e323f0278e8fea84da68b3ac8967ceee760
Anthony Langsworth <anthony.langsworth@global.ntt>
2021-02-27 17:02:23 +1100

    Allow reasonable errors to not stop deployment

--- deploy.cmd
+++ deploy.cmd
@@ -1,12 +1,12 @@
 set dest=%userprofile%\AppData\Local\EDMarketConnector\plugins\EDMFAT
-copy %dest%\settings.json %temp% || goto error
-rd /s /q %dest% || goto error
+copy %dest%\settings.json %temp%
+rd /s /q %dest%
 mkdir %dest% || goto error
 xcopy /y load.py %dest% || goto error
-xcopy /y web_services.py %dest% || goto error
+xcopy /y edmfat_web_services.py %dest% || goto error
 mkdir %dest%\edmfs || goto error
 xcopy /s /y edmfs\*.py %dest%\edmfs  || goto error
-copy %temp%\settings.json %dest% || goto error
+copy %temp%\settings.json %dest%
 
 goto end
 
