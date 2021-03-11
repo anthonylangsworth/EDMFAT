9726cd97d42f2435c98cab6b99db8a8583694735
Anthony Langsworth <anthony.langsworth@gmail.com>
2021-03-11 20:14:50 +1100

    Switch to local certificate store

--- wix/signmsi.cmd
+++ wix/signmsi.cmd
@@ -1,1 +1,1 @@
-signtool sign /tr http://timestamp.sectigo.com/ /fd sha256 /td sha256 /sm /sha1 13e3cc84a91445eda3cb88d4915c888120a823f1 /d "EDMFAT MSI" edmfat.msi
+signtool sign /tr http://timestamp.sectigo.com/ /fd sha256 /td sha256 /sha1 13e3cc84a91445eda3cb88d4915c888120a823f1 /d "EDMFAT MSI" edmfat.msi
