50aa92f8b7bc2b128c291e1cde27a115dd5160bf
Anthony Langsworth <anthony.langsworth@gmail.com>
2022-01-27 15:51:02 +1100

    Better list search, although Python still sucks at this

--- edmfs/market.py
+++ edmfs/market.py
@@ -7,7 +7,4 @@ def get_market_entry(commodity_name: str, market_json_file_path:str = None) -> D
     file_path = "%%userprofile%%\\Saved Games\\Frontier Developments\\Elite Dangerous\\market.json" if market_json_file_path == None else market_json_file_path
     with open(file_path, mode="r") as market_json_file:
         market = json.load(market_json_file)
-    for market_entry in market["Items"]:
-        if market_entry["Name_Localised"] == commodity_name:
-            return market_entry            
-    return None
+    return next(filter(lambda market_entry: market_entry["Name_Localised"] == commodity_name, market["Items"]), None)
